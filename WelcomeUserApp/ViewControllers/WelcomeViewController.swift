8071d3537428a041167180adc895d206af524587
Alex Rybachev <rybachev.as@gmail.com>
2022-03-08 00:09:12 +0400

    setup ViewControllers and UI-elements on its

---
+++ WelcomeUserApp/ViewControllers/WelcomeViewController.swift
@@ -0,0 +1,18 @@
+//
+//  WelcomeViewController.swift
+//  WelcomeUserApp
+//
+//  Created by Aleksandr Rybachev on 08.03.2022.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+      
+    }
+    
+}
